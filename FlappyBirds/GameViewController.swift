b09ffeae2828ae9d0ddbf1937672f86f3c1c089e
Supannee Mutitanon <supannee@dmit.co.th>
2018-12-16 01:29:53 +0700

    add files

---
+++ FlappyBirds/GameViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GameViewController.swift
+//  FlappyBirds
+//
+//  Created by Supannee Mutitanon on 4/6/18.
+//  Copyright © 2018 Supannee Mutitanon. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = false
+        skView.showsNodeCount = false
+        skView.ignoresSiblingOrder = false
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
